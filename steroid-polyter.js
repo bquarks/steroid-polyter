6da939ab702bea2ff4973b264db9a5456f109598
Francisco López <donflopez@gmail.com>
2015-08-26 11:34:38 +0200

    Fix replace state

--- steroid-polyter.js
+++ steroid-polyter.js
@@ -395,7 +395,7 @@ Polymer({
         if (options.silent === true) {
           this._triggered = true;
           page.show(route, undefined, undefined, false);
-          window.replaceState(undefined, undefined, route);
+          window.history.replaceState(undefined, undefined, route);
         }
         else if (options.trigger === false) {
           this._triggered = true;
